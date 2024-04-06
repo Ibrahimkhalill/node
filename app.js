8ad5435db3fac0f8a45baf8c45b96f5840dc09ad
Ibrahimkhalill <mdibrahimkhalil516@gmail.com>
2024-04-06 14:07:53 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Define routes
+app.get('/', (req, res) => {
+  res.render('index', { message: 'I love you Ibrahim' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { pageTitle: 'About' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { pageTitle: 'Contact Us' });
+});
+
+// ... (remaining code)
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
