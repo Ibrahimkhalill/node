d6126adda8f0d37e405b2bad19416c757cc50c8f
Ibrahimkhalill <mdibrahimkhalil516@gmail.com>
2024-04-06 14:19:27 +0600

    port changes

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 // app.js
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8001;
 
 // Set EJS as the view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Define routes
-app.get('/', (req, res) => {
-  res.render('index', { message: 'I love you Ibrahim' });
+app.get("/", (req, res) => {
+  res.render("index", { message: "I love you Ibrahim" });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', { pageTitle: 'About' });
+app.get("/about", (req, res) => {
+  res.render("about", { pageTitle: "About" });
 });
 
-app.get('/contact', (req, res) => {
-  res.render('contact', { pageTitle: 'Contact Us' });
+app.get("/contact", (req, res) => {
+  res.render("contact", { pageTitle: "Contact Us" });
 });
 
 // ... (remaining code)
